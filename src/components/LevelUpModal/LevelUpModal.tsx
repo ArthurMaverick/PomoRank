487a5306c39cc9f9b3be4fb4251752b70b4e9895
ArthurMaverick <arthuracs18@gmail.com>
2021-03-04 18:39:17 -0300

    levelUpModal container change from css to cssInJs

--- src/components/LevelUpModal/LevelUpModal.tsx
+++ src/components/LevelUpModal/LevelUpModal.tsx
@@ -1,21 +1,23 @@
 import { useContext } from 'react';
 import { challengesContext } from '../../contexts';
-import {style} from '../../stylus'
+import {Overlay, ContainerModal} from './styles'
+
 export const LevelUpModal = () => {
   const {level, closeLevelUpModal} = useContext(challengesContext);
+
   return (
-      <div className={style.LevelUpModal.overlay}>
-    <div className={style.LevelUpModal.containerModal}>
-      <header>{level}</header>
-      <strong>Parabens</strong>
-      <p>Voce passou de level</p>
+    <Overlay>
+      <ContainerModal>
+      
+        <header>{level}</header>
+        <strong>Parabens</strong>
+        <p>Voce passou de level</p>
 
-      <button type="button" onClick={closeLevelUpModal}>
-        <img src="/icons/close.svg" alt="Fechar Modal"/>
-      </button>
+        <button type="button" onClick={closeLevelUpModal}>
+          <img src="/icons/close.svg" alt="Fechar Modal"/>
+        </button>
 
-
-    </div>
-    </div>
+      </ContainerModal>
+    </Overlay>
   )
 }
--- src/stylus/List.ts
+++ src/stylus/List.ts
@@ -1,28 +1,17 @@
-import { LevelUpModal } from '../components/LevelUpModal/LevelUpModal';
-import stylesExperience from '../styles/components/ExperienceBar.module.css'
 import stylesProfile from '../styles/components/Profile.module.css'
-import styleLevelupModal from '../styles/components/levelupModal.module.css'
 
 
 function getStyles(){
-  const {experienceBar,currentExperience } =  stylesExperience 
-  const {containerModal, overlay} =   styleLevelupModal
   const {profileContainer} = stylesProfile
   
 
   const styles = {
-    experience: { 
-      experienceBar,
-      currentExperience
-   },
+   
     profile: {
       profileContainer
    },
    
-    LevelUpModal: {
-      containerModal,
-      overlay
-    }
+   
   
   }
 
