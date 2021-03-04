2684cd30873c4bc5377d225722b1da2fe1c39143
ArthurMaverick <arthuracs18@gmail.com>
2021-03-04 19:54:15 -0300

    add styled losted container change from css to cssInJs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import {ThemeProvider} from 'styled-components'
-import {ColorsProvider} from '../styles/global/styles.themeProvider'
-import { CountdownProvider } from '../contexts'
+import {ColorsProvider} from '../styles/global'
+import {GlobalStyle} from '../styles/global'
+import {CountdownProvider} from '../contexts'
 import {Provider} from 'next-auth/client'
-import {GlobalStyle} from '../styles/global/style.global'
 
 function MyApp({ Component, pageProps }) {
   return (
