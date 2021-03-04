f82b17ee9916729db5f2f695e1e0b63b490cced6
ArthurMaverick <arthuracs18@gmail.com>
2021-03-04 18:50:17 -0300

    profile container change from css to cssInJs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,12 @@
 import {useContext} from 'react'
-import {style} from '../../stylus'
+import {ProfileContainer} from './styles'
 import {challengesContext} from '../../contexts'
 export const Profile = () => {
 
   const {level} = useContext(challengesContext)
 
   return (
-    <div className={style.profile.profileContainer}>
+    <ProfileContainer>
       <img src="https://github.com/ArthurMaverick.png" alt="Arthur"/>
       <div>
           <strong>Arthur Santos</strong>
@@ -16,6 +16,6 @@ export const Profile = () => {
           </p>
         </div>
       
-    </div>
+    </ProfileContainer>
   )
 }
